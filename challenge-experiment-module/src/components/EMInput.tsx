d44e53e60764e89fd7011b23e72a2dda2566245d
Muzammil Hussain Shah <mynameismuzammilhussainshah@gmail.com>
2024-04-16 21:23:54 +0500

    feat: add generate one button

--- challenge-experiment-module/src/components/EMInput.tsx
+++ challenge-experiment-module/src/components/EMInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 export const EMInput = ({ item, index }: any) => {
     const [addIteration, setAddIteration] = useState(false)
     const [moduleName, setmoduleName] = React.useState<string>('')
@@ -12,6 +12,12 @@ export const EMInput = ({ item, index }: any) => {
             <div key={index.toString()} className='input_container'>
                 <p className='input_title'>EM-{index + 1}</p>
                 <p className='input_title'>{item}</p>
+                <div
+                    style={{ display: 'flex', justifyContent: "flex-end", alignItems: "center", flex: 1, marginRight:"16px"}}>
+
+                    <p className='input_title'>Selection</p>
+                    <FiberManualRecordIcon style={{ color: 'green', fontSize: 14 }} />
+                </div>
             </div>
         </>
     )
--- challenge-experiment-module/src/components/experimentalModuleCart.tsx
+++ challenge-experiment-module/src/components/experimentalModuleCart.tsx
@@ -6,6 +6,7 @@ export const ExperimentalModuleCart = () => {
     const [isOpen, setIsOpen] = React.useState(false)
     const [moduleName, setmoduleName] = React.useState<string>('')
     const [addIteration, setaddIteration] = React.useState<boolean>(true)
+    const [generateOne, setgenerateOne] = React.useState<boolean>(false)
     const [experimentalModules, setexperimentalModules] = React.useState<any>([])
     const handleInputChange = (event) => {
         setmoduleName(event.target.value);
@@ -42,23 +43,25 @@ export const ExperimentalModuleCart = () => {
                         {addIteration &&
                             <div className='input_container' style={{ margin: '5px auto' }}>
                                 <p className='input_title'>EM-{experimentalModules.length + 1}</p>
-                                <input
-                                    value={moduleName}
-                                    onChange={handleInputChange}
-                                    type="text"
-                                    placeholder="Adding iteration"
-                                    className="custom-input"
-                                />
+                                {!generateOne ?
+                                    <input
+                                        value={moduleName}
+                                        onChange={handleInputChange}
+                                        type="text"
+                                        placeholder="Adding iteration"
+                                        className="custom-input"
+                                    />
+                                    :
+                                    <div style={{ backgroundColor: `red` ,height:`100px`,width:`75%`}}></div>}
                             </div>
                         }
                     </>
 
                 }
                 <div className='prompt_container'>
-                    <p className='input_title'>To add a new iteration, start typing a<br /> prompt or <span className='under_line'>generate</span> one.</p>
+                    <p className='input_title'>To add a new iteration, start typing a<br /> prompt or <span className='under_line generate_button' onClick={() => setgenerateOne(true)}>generate</span> one.</p>
                 </div>
                 {addIteration ?
-                    // {experimentalModules.length > 0 ?
                     <div className='footer_buttons_container'>
                         <p className='input_title footer_buttons' onClick={() => setIsOpen(false)}>Cancel</p>
                         <p className='input_title footer_buttons' onClick={() => {
