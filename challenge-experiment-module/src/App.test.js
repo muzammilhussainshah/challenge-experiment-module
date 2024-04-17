51bc2a9d04aaf0f83856aba0830d18e53860d03d
Muzammil Hussain Shah <35377074+muzammilhussainshah@users.noreply.github.com>
2024-04-18 02:58:05 +0500

    feat: testcases add

--- challenge-experiment-module/src/App.test.js
+++ challenge-experiment-module/src/App.test.js
@@ -1,24 +1,59 @@
+/* eslint-disable */
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
+import path from 'path' // Import path module
+import fs from 'fs' // Import file system module
 
 describe('App Component', () => {
-  // Test to ensure that the App component renders without crashing
+  // RENDER APP WITHOUT CRASHING
   test('renders without crashing', () => {
     render(<App />)
   })
-
-  // Test to ensure that the "Create Experiment Module" is rendered
+  // Experiment MODULE COMPONENT RENDER Ï
   test('renders Create Experiment Module', () => {
     const { getByText } = render(<App />)
     const createModule = getByText('Create Experiment Module')
     expect(createModule).toBeInTheDocument()
   })
+})
 
-  // Test to ensure that the "Experiment Module List" is rendered
-  test('renders Experiment Module List', () => {
-    const { getByText } = render(<App />)
-    const experimentList = getByText('Experiment Module')
-    expect(experimentList).toBeInTheDocument()
+// CHECK INVALID COLOR FROM APP
+describe('Color Usage Test', () => {
+  test('checks if any other color is used in the app', () => {
+    const { container } = render(<App />)
+    const elementsWithStyles = container.querySelectorAll('[style]')
+    elementsWithStyles.forEach(element => {
+      const style = element.getAttribute('style')
+      if (style.includes('color') || style.includes('background-color')) {
+        if (
+          !style.includes('#191919') &&
+          !style.includes('#000000') &&
+          !style.includes('#757575') &&
+          !style.includes('green') &&
+          !style.includes('rgb(117, 117, 117)') &&
+          !style.includes('#FFFFFF')
+        ) {
+          console.log('Element with different color:', element, 'Style:', style)
+          expect(true).toBe(false)
+        }
+      }
+    })
+
+    // Pass the test if no other color is found
+    expect(true).toBe(true)
+  })
+})
+
+// CHECK INVALID COLOR FROM CSS FILE
+describe('CSS Color Test', () => {
+  test('Check if only specified colors are used in CSS file', () => {
+    const cssFilePath = path.resolve(__dirname, './App.css')
+    const cssFileContent = fs.readFileSync(cssFilePath, 'utf8')
+    const expectedColors = ['#191919', '#000', 'rgb(117, 117, 117)', '#757575', '#FFFFFF', 'green']
+    const regex = /#[0-9A-Fa-f]{3,6}|rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)|\b(?:red|blue|green|yellow|orange|purple|pink)\b/g
+    const foundColors = cssFileContent.match(regex) || []
+    const unexpectedColors = foundColors.filter(color => !expectedColors.includes(color))
+    expect(unexpectedColors).toHaveLength(0)
   })
 })
